015cd73ed47f1a20f592b6a628c9417cede9f662
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 22:03:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
   return false if number <= 1
   my_array = (2..number).to_a
-  my_array.each do |x, y|
+  my_array.each_with_index do |x, y|
     return false if number % x == 0 && x != number
   end
   return true
