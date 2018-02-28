5965fcff06c4000d47f24080c97c53e8a1fa8aa1
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 21:07:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number <= 1
+  my_array = (1..number).to_a
+  my_array.each do |x|
+    x = x - 1
+    return false if number % my_array[x] == 0 && my_array[x] != 1 && my_array[x] != number
+  end
+  return true
+end
